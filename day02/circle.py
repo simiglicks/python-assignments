0d519cd591ed9621f02961fc8ceeb166077267d8
Simi Glicksman <simiglicks@gmail.com>
2024-11-13 16:58:08 +0200

    day02_assignment

---
+++ day02/circle.py
@@ -0,0 +1,5 @@
+def circle(radius):
+    area= 3.14*(radius**2)
+    circumference= 3.14*(2*radius)
+    print(area)
+    print(circumference)
---
+++ day02/rectangle.py
@@ -0,0 +1,7 @@
+
+def rectangle(width, length):
+    area= width*length
+    perimeter= 2*width + 2*length
+    print(area)
+    print(perimeter)
+    #i did perimeter not circumference
